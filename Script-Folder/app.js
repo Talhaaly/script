548cdabbf1b8a381cbf8cdba967f0cf2f1ad9d0d
talha <talhaaly103@gmail.com>
2023-08-15 16:22:43 +0500

    java practice

---
+++ Script-Folder/app.js
@@ -0,0 +1,88 @@
+// My Practice
+// alert function / document.write / console.log 
+document.write("<br>");
+alert('Error please Enter a valid Password')
+document.write('<br> this is my web page.')
+document.write("<br>");
+document.write('<br> welcome to JS Land..... <br> Happy Coding')
+alert('first message alert')
+alert('second message alert')
+document.write("<br>");
+console.log('happy coding....')
+var username;
+var myName=`Talha Ali Akber`
+
+// add operation
+document.write("<br>");
+var a,b;
+a=12;
+b=13;
+var add;
+add=a+b;
+
+// subtract operation
+document.write("<br>");
+var a,b,c;
+a=100;
+b=8;
+c=6;
+var sub=a-b-c;
+
+// test
+document.write("<br>");
+var a=2,b=1;
+var result= --a - --b + ++b + b--;
+document.write("Test result:" + " " + result);
+
+// total 
+document.write("<br>");
+var num1=5; 
+var num2=6;
+var total= num1 + num2;
+document.write("<br> the sum of 5 & 6 is" + " " + total  + "<br>")
+
+// percentage 
+document.write("<br>");
+var obtainMark=779;
+var totalMark=850;
+document.write('obtain mark is 779' + '<br>');
+document.write('total mark is 850' + '<br>');
+var percentage = (obtainMark/totalMark) * 100;
+document.write("percentage &nbsp;" + " " + percentage + "%" + "<br>");
+
+// Table 
+document.write("<br>");
+document.write('Table of Six'+ "<br>");
+var table;
+table=6*1;
+document.write('6*1=' + " " + table + "<br>")
+table=6*2;
+document.write('6*2=' + " " + table + "<br>")
+table=6*3;
+document.write('6*3=' + " " + table + "<br>")
+table=6*4;
+document.write('6*4=' + " " + table + "<br>")
+table=6*5;
+document.write('6*5=' + " " + table + "<br>")
+table=6*6;
+document.write('6*6=' + " " + table + "<br>")
+table=6*7;
+document.write('6*7=' + " " + table + "<br>")
+table=6*8;
+document.write('6*8=' + " " + table + "<br>")
+table=6*9;
+document.write('6*9=' + " " + table + "<br>")
+table=6*10;
+document.write('6*10=' + " " + table + "<br>")
+
+// Task 
+document.write("<br>");
+var first=23;
+var second=32;
+document.write("<br>");
+var calculation=(first/second) * 100;
+document.write('Results:' + " " + calculation + "<br>");
+document.write("<br>");
+calculation -=1;
+
+document.write('Results:' + " " + calculation + "<br>")
