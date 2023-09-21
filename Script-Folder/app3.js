cdab5529212dbff6e41f731e5fed2a52dc70e522
talha <talhaaly103@gmail.com>
2023-09-21 14:18:37 +0500

    for loop

---
+++ Script-Folder/app3.js
@@ -0,0 +1,31 @@
+// var a=10;
+// var b=20;
+// if(a < b){
+//     alert("Correct");
+// };
+
+// var c=10;
+// var d=10;
+// if(c==d){
+//     document.write("syntex is right")
+// }
+
+// document.write('<br>')
+// var teacher=prompt("teacher is sufiyan")
+// if(teacher.toLowerCase("sufiyan")=="sufiyan"){
+//     document.write('yes your teacher is sufiyan');
+// }
+// else{
+//     document.write("not your teacher");
+// };
+
+// =================x===============x===================== 
+
+
+var geek = ["Ali", "Talha", "Saim", 56, "Sultan"];
+
+for (let i = 0; i < geek.length; i++) {
+    console.log(i);
+}
+
+
