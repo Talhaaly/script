be560ca8d211fd171ad1ba5e104182e43486341a
talha <talhaaly103@gmail.com>
2023-10-03 12:28:49 +0500

    Array's Basic

---
+++ Array's/array.js
@@ -0,0 +1,91 @@
+// Array  Basics
+
+
+// const myArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// console.log(myArr[2]);
+
+// const myArr2 = new Array (0, 1, 2, 3, 4, 5, 6)
+
+// const myHeros = ["Shahrukh", "Salman", "Amir"];
+
+// console.log(myHeros[1]);
+
+
+// Array Methods 
+
+
+
+// push and pop 
+
+
+
+// myArr.push(16)
+// myArr.push(13)
+// myArr.pop()
+// console.log(myArr);
+
+
+
+// shift and unshift 
+
+
+
+// myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr);
+
+
+
+// includes and indexof 
+
+
+
+// console.log(myArr.includes(8))
+// console.log(myArr.includes(11))
+
+
+// console.log(myArr.indexOf(8))
+// console.log(myArr.indexOf(11))
+
+
+// join 
+
+
+// const newArr = myArr2.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+
+// slice and splice 
+
+
+
+// console.log("A", myArr)
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1)
+// console.log("B", myArr)
+
+
+// const myn2 = myArr.splice(1,3)
+// console.log("C", myArr)
+// console.log(myn2);
+
+
+// difference b/w slice & splice  
+// slice main range include nhi hoti or old array wese e rehta hay
+
+// splice main range include hoti hay or old array main say part nikal lyta hay ot old array me splice kai gaye item hat jaty hai. 
+
+
+
+
+
+
+
