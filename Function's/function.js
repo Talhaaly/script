1259bc20e2bd59928cf102b6e7f7305ab8b36c11
talha <talhaaly103@gmail.com>
2023-10-05 12:02:18 +0500

    function

---
+++ Function's/function.js
@@ -0,0 +1,128 @@
+
+// function 
+
+
+// function myName(){
+    
+//     console.log("T");
+//     console.log("A");
+//     console.log("L");
+//     console.log("H");
+//     console.log("A");
+    
+// }
+
+// myName()
+
+
+
+// function uppercase(str){
+
+// return str.toUpperCase();
+// }
+
+// const result = uppercase("javascript");
+
+// console.log(result)
+
+
+
+function calculateArea ( width, height) {
+    const area = width * height
+    return area
+}
+
+const result = calculateArea(2,5)
+
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
