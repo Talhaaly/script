8a1e4d6a7c46c6ac21dfa3c4a769b968996cfde0
talha <talhaaly103@gmail.com>
2023-10-09 10:40:01 +0500

    Ass

--- Assignments21/Ass.js
+++ Assignments21/Ass.js
@@ -22,3 +22,58 @@
 
 // const phoneModel = prompt('What Is Your Favorite mobile Phone Moldel ?')
 // console.log(phoneModel.length);
+
+
+
+
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser.
+
+
+
+
+// const country = "PAKISTANI";
+// const countryIndexNo = country.indexOf("N");
+// console.log(countryIndexNo);
+
+
+
+
+
+
+
+// 4. Write a program to find the last index of letter “l” in the 
+// word “Hello World” and display the result in your browser
+
+
+
+
+
+// const text = "Hello World";
+// const result = text.lastIndexOf("l")
+// console.log(result);
+
+
+
+
+
+
+
+// 5. Write a program to find the character at 3rd index in the 
+// word “Pakistani” and display the result in your browser.
+
+
+
+
+
+
+// const names = "Pakistani";
+// console.log(names[3]);
+
+
+
+
+
+
+
