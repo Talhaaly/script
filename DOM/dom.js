c5c7fc48765f8e309c94544726c4880312622d96
talha <talhaaly103@gmail.com>
2024-01-10 15:52:54 +0500

    DOM-ps

---
+++ DOM/dom.js
@@ -0,0 +1,12 @@
+// alert('Hello World');
+
+
+// console.dir(window.document)
+
+
+// console.log(document.body);
+// console.log(document.body.childNodes[1]);
+// console.log(document.body.childNodes[1].firstChild);
+
+
+
