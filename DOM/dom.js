0c743d4c589a2ef91bc83bedf2b0fe621186a04b
talha <talhaaly103@gmail.com>
2024-01-12 11:40:29 +0500

    add events by DOM

--- DOM/dom.js
+++ DOM/dom.js
@@ -9,5 +9,48 @@
 // console.log(document.body.childNodes[1].firstChild);
 
 
-let dom = document.getElementsByClassName('dom')
-console.log(dom);
+// let dom = document.getElementsByClassName('dom')
+// console.log(dom);
+
+
+// 1. Selecting Elements:
+// Selecting an element by its ID
+// var element = document.getElementById('myId');
+
+// Selecting elements by their class name
+// var elements = document.getElementsByClassName('myClass');
+
+// Selecting elements by their tag name
+// var elements = document.getElementsByTagName('myTag');
+
+
+
+
+
+// 2. Adding Events:
+// Adding a click event to an element
+// element.addEventListener('click', function() {
+//     console.log('Element was clicked!');
+//   });
+  
+  // Adding a mouseover event to an element
+//   element.addEventListener('mouseover', function() {
+//     console.log('Mouse is over the element!');
+//   });
+
+
+
+
+
+// 3. Manipulating Elements:
+// Changing the content of an element
+// element.textContent = 'New content!';
+
+// Adding a class to an element
+// element.classList.add('newClass');
+
+// Removing a class from an element
+// element.classList.remove('oldClass');
+
+// Toggling a class on an element
+// element.classList.toggle('toggleClass');
