b2d49f042fde25ad9c814e354ddd432eebafe0ff
talha <talhaaly103@gmail.com>
2024-01-29 10:24:48 +0500

    jsW

--- Array's/array.js
+++ Array's/array.js
@@ -192,3 +192,23 @@
 
 // let arr = [1, 5, 2, 4, 3];
 // arr.sort(); // arr is now [1, 2, 3, 4, 5]
+
+
+// ==========================================================================
+
+// let arr = [1,2,3,4,5,6,7,8];
+//  arr.forEach(element => {
+//      let sum = element * 2
+//      console.log(sum);
+// });
+
+// console.log(arr);
+
+
+// const bioData = {
+// name : "Talha",
+// age : 23,
+// class : "web & app"
+// }
+
+// console.log(bioData);
