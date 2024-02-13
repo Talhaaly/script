c564713122cbfa9a00f6df41c77028f8dc22d077
talha <talhaaly103@gmail.com>
2024-02-13 13:49:33 +0500

    JS

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+// const myArr =['ali','talha','saim','akber'] 
+// const [one,two,three,four] = myArr
+// console.log(one);
+
+
+
+
+// const myObj = {
+//     name : "talha",
+//     edu : "10th",
+//     age : 23
+// }
+
+// const {name,edu} = myObj
+// console.log(edu);
+
+
+
+// const ourArr = [1,2,3,4,5,6,7,8,9]
+// const newArr = ourArr.map(num => num *2)
+// console.log(newArr);
