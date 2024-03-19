d63ca6415d4d6615abbfd3e94e0c394fc0df6503
talha <talhaaly103@gmail.com>
2024-03-19 12:49:23 +0500

    Array-Method

--- Array's/array.js
+++ Array's/array.js
@@ -352,5 +352,10 @@
 
 
 
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// console.log(sum); // Output: 15
 
 
