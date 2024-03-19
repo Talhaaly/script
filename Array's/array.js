a2eb61952e0d1368145abc51537b6a6612e418fc
talha <talhaaly103@gmail.com>
2024-03-19 12:48:34 +0500

    Method

--- Array's/array.js
+++ Array's/array.js
@@ -332,6 +332,24 @@
 // console.log(color1, color2, color3); // Output: red green blue
 
 
+// function loopExample() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+// }
+
+// loopExample();
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const squaredNumbers = numbers.map(num => num * num);
+
+// console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+
+
 
 
 
