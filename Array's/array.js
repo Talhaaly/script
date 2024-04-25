45cd6b6eae3074557edd7ea9701f0ed24686e484
talha <talhaaly103@gmail.com>
2024-04-25 11:08:54 +0500

    fun

--- Array's/array.js
+++ Array's/array.js
@@ -525,7 +525,10 @@
 
 
 
-
+// fetch('https://api.example.com/data')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error));
 
 
 
